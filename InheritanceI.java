8eb9b8fca258ebb9d015f75e6a95a97cb852cbee
Bhakya26 <bhakyagajendran@gmail.com>
2025-05-23 20:36:17 +0530

    Create InheritanceI.java

---
+++ InheritanceI.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+class Animal{
+    void walk(){
+        System.out.println("I am walking");
+    }
+}
+class Bird extends Animal {
+    void fly() {
+        System.out.println("I am flying");
+    }
+    void sing(){
+        System.out.println("I am singing");
+    }
+}
+public class InheritanceI {
+
+    public static void main(String[] args) {
+        Bird b=new Bird();
+        b.walk();
+        b.fly();
+        b.sing();
+    }
+}
